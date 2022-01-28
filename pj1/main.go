7fd6d6aa63911f587edcac15b2890abca0ae7f92
Liberbell <liberbell@gmail.com>
2022-01-28 23:06:05 +0900

    main1

--- pj1/main.go
+++ pj1/main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 
 	"example.local/pj1/pj1/math"
-	"example.local/pj1/pj1/math/stats"
+	// "example.local/pj1/pj1/math/stats"
 )
 
 func main() {
 	vals := []float64{1, 10, 100, 1000}
 	fmt.Println(math.Add(10, 20))
-	fmt.Println(stats.Avg(vals))
+	fmt.Println(math.Avg(vals))
 }
