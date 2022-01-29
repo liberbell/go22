074a6ff89abfe7984293f2905ca6ab71eb61892f
Liberbell <liberbell@gmail.com>
2022-01-29 10:10:16 +0900

    main2

--- pj1/main.go
+++ pj1/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
+	f "fmt"
 	"math"
-	"os"
+	. "os"
 	_ "strings"
 
 	m "example.local/pj1/pj1/math"
@@ -12,9 +12,9 @@ import (
 
 func main() {
 	// vals := []float64{1, 10, 100, 1000}
-	fmt.Println(m.Add(10, 20))
+	f.Println(m.Add(10, 20))
 	// fmt.Println(math.Avg(vals))
-	fmt.Println(math.Pi)
-	fmt.Println(m.Pi)
-	fmt.Fprintln(os.Stdout, "Hello")
+	f.Println(math.Pi)
+	f.Println(m.Pi)
+	f.Fprintln(Stdout, "Hello")
 }
