c6d84d41df47dc0f59a77b4cabdb8093646fa511
Liberbell <liberbell@gmail.com>
2022-01-31 08:45:59 +0900

    main3

--- pj6/funcval/main.go
+++ pj6/funcval/main.go
@@ -1,10 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 	anon := func(msg string) bool {
 		fmt.Println(msg)
 		return true
 	}
+	res := anon("Hello anonymous function!")
+	fmt.Println(res)
+	fmt.Println("anon is of tyep: ", reflect.TypeOf(anon))
 }
