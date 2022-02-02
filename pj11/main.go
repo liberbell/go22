23ec7eae65da85e9fc28d878d7d21b7b806db087
Liberbell <liberbell@gmail.com>
2022-02-03 08:53:34 +0900

    main2

--- pj11/main.go
+++ pj11/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "runtime"
+import (
+	"runtime"
+	"sync"
+)
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	var counter uint64
+	var wg sync.WaitGroup
+
+	inc := func() {
+		for i := 0; i < 1000; i++ {
+			counter++
+		}
+	}
+
+	dec := func() {
+		for i := 0; i < 1000; i++ {
+			counter--
+		}
+	}
 }
