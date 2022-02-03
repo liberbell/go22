d98d205dc0d10015c031c4fec27b9c3b02c0a803
Liberbell <liberbell@gmail.com>
2022-02-03 09:04:55 +0900

    main3

--- pj12/main.go
+++ pj12/main.go
@@ -10,4 +10,14 @@ func main() {
 
 	var counter uint64
 	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func()  {
+			defer wg.Done()
+			for j := 0; j < 1000; j++ {
+				counter++
+			}
+		}
+	}
 }
