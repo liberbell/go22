4d6bedaadcefcb9eef635d541c7647b4c543515b
Liberbell <liberbell@gmail.com>
2022-02-04 09:06:00 +0900

    main7

--- pj13/main.go
+++ pj13/main.go
@@ -11,4 +11,5 @@ func main() {
 	val := <-ch
 	fmt.Printf("type of value of ch is: %T\n", ch)
 	fmt.Printf("the value of ch is: %v\n", ch)
+	fmt.Printf("the resulting value from the goroutine is: %\n", val)
 }
