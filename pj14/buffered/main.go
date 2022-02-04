28625f739bcaceafcaf22612605389fe2bdf1d7b
Liberbell <liberbell@gmail.com>
2022-02-04 14:27:56 +0900

    main3

--- pj14/buffered/main.go
+++ pj14/buffered/main.go
@@ -6,4 +6,9 @@ func main() {
 	ch := make(chan string)
 	go send(ch)
 	fmt.Println("\nblocking and send goroutine...")
+	fmt.Printf("channel length: %v\n", len(ch))
+}
+
+func send(ch chan string) {
+
 }
