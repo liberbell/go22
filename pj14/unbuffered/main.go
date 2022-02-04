98e32cf6c744a00181eb711f4a40085a186afab0
Liberbell <liberbell@gmail.com>
2022-02-04 14:29:03 +0900

    main1

---
+++ pj14/unbuffered/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan string)
+	go send(ch)
+	fmt.Println("\nblocking and send goroutine...")
+	fmt.Printf("channel length: %v\n", len(ch))
+	time.Sleep(time.Second * 2)
+}
+
+func send(ch chan string) {
+
+}
