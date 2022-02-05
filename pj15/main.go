a9198b4e0165acab7fa20159025e1a67692e79c3
Liberbell <liberbell@gmail.com>
2022-02-05 16:59:23 +0900

    main2

--- pj15/main.go
+++ pj15/main.go
@@ -1,5 +1,15 @@
 package main
 
+import "strconv"
+
 func main() {
 	ch := make(chan string, 2)
+
+	go func(ch chan string) {
+		defer close(ch)
+		for i := 0; i < 5; i++ {
+			msg := "message" + strconv.Itoa(i)
+			ch <- msg
+		}
+	}()
 }
