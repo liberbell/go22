bc0d42c95f43856c1c91f6a9b71e37317a420179
Liberbell <liberbell@gmail.com>
2022-02-06 20:41:02 +0900

    main2

--- pj17/main.go
+++ pj17/main.go
@@ -1,6 +1,16 @@
 package main
 
+import "time"
+
 func main() {
 	ch1 := make(chan int)
 	ch2 := make(chan int)
+
+	go func() {
+		for i := 1; i < 4; i++ {
+			time.Sleep(3 * time.Second)
+			ch1 <- i
+			time.Sleep(1 * time.Second)
+		}
+	}()
 }
