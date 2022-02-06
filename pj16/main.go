6169300deb62ceb783aa38e34c054fc02aa9cf05
Liberbell <liberbell@gmail.com>
2022-02-06 20:38:17 +0900

    main9

--- pj16/main.go
+++ pj16/main.go
@@ -11,17 +11,17 @@ func main() {
 	ch3 := make(chan int)
 
 	go func() {
-		time.Sleep(5 * time.Second)
+		time.Sleep(8 * time.Second)
 		ch1 <- 1
 	}()
 
 	go func() {
-		time.Sleep(5 * time.Second)
+		time.Sleep(8 * time.Second)
 		ch2 <- 2
 	}()
 
 	go func() {
-		time.Sleep(5 * time.Second)
+		time.Sleep(8 * time.Second)
 		ch3 <- 3
 	}()
 
